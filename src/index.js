b9f8ecf250139fd039b3bedaa0445d312c0c3278
kisstamasj <48266482+kisstamasj@users.noreply.github.com>
2023-03-11 12:29:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -111,10 +111,10 @@ function render(str, options) {
 
 function getBullet(lvl) {
     switch (lvl) {
-        case 1: return '##';
-        case 2: return '-';
-        case 3: return '*';
-        case 4: return '+';
+        case 1: return '-';
+        case 2: return '*';
+        case 3: return '+';
+        case 4: return '-';
         default: return '';
     }
 }
